b3405039f1558a6450cfda091a514490e2475c97
Sriniketh J <srinikethcr7@gmail.com>
2024-06-13 20:35:29 +0530

    chore: update who am i

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -8,24 +8,27 @@ const AboutContent = () => {
       <div className="left">
         <h1>Who am I?</h1>
         <p>
-          I am a Junior at PSG Institute of Technology and Applied Research
-          pursuing a B.E. in Electronics and Communication Engineering📚. A Data
-          Science & Machine Learning Enthusiast♥️. I am a life-long learner📖. I
-          always try to grab all the opportunities my way and participate in
-          many events without being deprived of the fact of losing💪. I also
-          write technical blogs in the areas of DS, ML, & WebDev👩‍💻. I like to
-          implement what I learned through building projects and participating
-          in hackathons👨‍🔧. I am exploring areas of Deep Learning and Web Dev as
-          well🧬. I like to play Football a lot ⚽. I am learning German as
-          well🗣️.
+          A passionate software and ML developer enthusiastic about the latest
+          advancements. Always keen to learn and be curious. I always like to
+          upskill myself. Ask me about open-source, AI-ML, software dev, and
+          anything in general. Always willing to collaborate and contribute to
+          the community. I'm inculcating the habit of reading books. I like to
+          play füßall a lot and learning deutsch as well.
         </p>
-        <a href="https://github.com/srini047/portfolio/blob/master/src/assets/Sriniketh_Resume.pdf" target="_blank" rel="noreferrer">
+        <a
+          href="https://github.com/srini047/portfolio/blob/master/src/assets/Sriniketh_Resume.pdf"
+          target="_blank"
+          rel="noreferrer"
+        >
           <button className="btn">Resume</button>
         </a>
       </div>
       <div className="right">
         <div className="img-container">
-          <img src="https://user-images.githubusercontent.com/81156510/230940009-bfcfa80e-4a9b-4e4f-842d-d3bb1f5dd521.png" alt="tech stack" />
+          <img
+            src="https://user-images.githubusercontent.com/81156510/230940009-bfcfa80e-4a9b-4e4f-842d-d3bb1f5dd521.png"
+            alt="tech stack"
+          />
         </div>
       </div>
     </div>
