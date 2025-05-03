d1ea6bb9c7365ba011c4b56a56d782fdc933ecff
Sriniketh J <srinikethcr7@gmail.com>
2025-05-03 13:44:52 +0530

    add: latest project

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -1,60 +1,75 @@
 const ProjectCardData = [
-    {
-        imgsrc: "https://user-images.githubusercontent.com/81156510/219946458-0481d137-1111-4f67-bd74-59263958abf8.png",
-        title: "HackProInit",
-        text: "An AI-powered one-stop platform to help kickstart your next hackathon/college project. Utilizes the power of Cohere, Twilio, GitHub, and OpenAI. Made use of React.js, Node.js, Express.js, Ant Design, Bootstrap.",
-        view: "https://devpost.com/software/hackproinit",
-        source: "https://github.com/srini047/hackathon-projects-backend"   
-    },
-    {
-        imgsrc: "https://user-images.githubusercontent.com/81156510/261383111-38dfc349-8ac9-4cc7-9e75-eb7521a7db18.png",
-        title: "Fusion Futurists",
-        text: "Revolutionize e-commerce with AI-driven personalization for trend-aligned & pleasing experience",
-        view: "https://srini047.github.io/walmart-sparkathon-2023/",
-        source: "https://github.com/srini047/walmart-sparkathon-2023"
-    },
-    {
-        imgsrc: "https://user-images.githubusercontent.com/81156510/151658225-7c2df2c6-a9ab-454e-991c-62eaf730d96c.png",
-        title: "HackTest API",
-        text: "A REST API built to fetch all the details regarding latest Hackathons and Coding Contests",
-        view: "https://github.com/srini047/HackTest",
-        source: "https://hacktestapi.herokuapp.com"
-    },
-    {
-        imgsrc: "https://user-images.githubusercontent.com/81156510/275876808-f1126ae6-94fc-4d18-ba29-7b623edace66.png",
-        title: "Salient Feature System",
-        text: "Detect profanity in the sentences and perform saliency checking on a sentence-by-sentence basis",
-        view: "https://huggingface.co/spaces/srini047/asapp-hackathon",
-        source: "https://github.com/srini047/asapp-hackathon"
-    },
-    {
-        imgsrc: "https://user-images.githubusercontent.com/81156510/213933187-2200364d-c15f-449c-a2e2-bfa563cccd18.png",
-        title: "Parking Assistant using OpenCV",
-        text: "Implemented a parking assistant that helps in finding parking lots in congested places using OpenCV, Mask RCNN, Twilio. Winner of Twilio Track Prize – Hack6iX                                                             ",
-        view: "https://devpost.com/software/parking-assistant-using-opencv",
-        source: "https://github.com/srini047/Hack6iX"
-    },
-    {
-        imgsrc: "https://user-images.githubusercontent.com/81156510/180809076-f4426ece-c8d5-492e-b44f-a43177a5afb9.png",
-        title: "Recommendation Engine",
-        text: "Designed and implemented price recommendation application using Web Scraping, in a 2-person team using Python. Enables buyers to select the best product with a smaller number of clicks from Amazon and Flipkart; Finalist in Tech Expo, an annual Techfest Techniche, conducted by IIT Guwahati – 2021 (Online)",
-        view: "https://github.com/srini047/price-recommender-engine",
-        source: "https://github.com/srini047/price-recommender-engine"
-    },
-    {
-        imgsrc: "https://user-images.githubusercontent.com/81156510/180811822-1476b05d-d389-4065-9a9d-26e366aa9f68.jpg",
-        title: "Sentiment Analysis using Python",
-        text: "Designed and implemented a text sentiment analyzer which classifies the nature of text as positive or negative using NLP. Runner Up in Hackathon, an annual Techfest Fiestaa 22, conducted by KPRIET – 2022 (In-person)",
-        view: "https://huggingface.co/spaces/srini047/text-based-sentiment-analyzer",
-        source: "https://github.com/srini047/text-based-sentiment-analysis"
-    },
-    {
-        imgsrc: "https://user-images.githubusercontent.com/81156510/221420531-489f1381-e185-4630-965f-111d7d607c66.png",
-        title: "Travelogue",
-        text: "One-stop platform to connect with guides, & tourists to discover, explore, and interact with native folks and their culture. Used MERN stack to build the application and made use of Cohere and Twilio APIs to enhance the user experience and provide an AI touch to the application.",
-        view: "http://devpost.com/software/travelogue-q1eyzh",
-        source: "https://github.com/srini047/travelogue-backend"   
-    },
-]
+  {
+    imgsrc: "https://i.ibb.co/S4zvhMzN/Screenshot-2025-05-03-at-1-31-05-PM.png",
+    title: "Email.AI",
+    text: "An AI-powered Email Assistant that helps you write emails faster and more effectively. It uses the power of OpenAI's GPT-3 to generate email content based on your input. Made use of Next.js, Typescript, and Shadcn.",
+    view: "https://email-ai-generator.vercel.app/",
+    source: "https://github.com/LLM-Projects/email.ai",
+  },
+  {
+    imgsrc:
+      "https://user-images.githubusercontent.com/81156510/219946458-0481d137-1111-4f67-bd74-59263958abf8.png",
+    title: "HackProInit",
+    text: "An AI-powered one-stop platform to help kickstart your next hackathon/college project. Utilizes the power of Cohere, Twilio, GitHub, and OpenAI. Made use of React.js, Node.js, Express.js, Ant Design, Bootstrap.",
+    view: "https://devpost.com/software/hackproinit",
+    source: "https://github.com/srini047/hackathon-projects-backend",
+  },
+  {
+    imgsrc:
+      "https://user-images.githubusercontent.com/81156510/261383111-38dfc349-8ac9-4cc7-9e75-eb7521a7db18.png",
+    title: "Fusion Futurists",
+    text: "Revolutionize e-commerce with AI-driven personalization for trend-aligned & pleasing experience",
+    view: "https://srini047.github.io/walmart-sparkathon-2023/",
+    source: "https://github.com/srini047/walmart-sparkathon-2023",
+  },
+  {
+    imgsrc:
+      "https://user-images.githubusercontent.com/81156510/151658225-7c2df2c6-a9ab-454e-991c-62eaf730d96c.png",
+    title: "HackTest API",
+    text: "A REST API built to fetch all the details regarding latest Hackathons and Coding Contests",
+    view: "https://github.com/srini047/HackTest",
+    source: "https://hacktestapi.herokuapp.com",
+  },
+  {
+    imgsrc:
+      "https://user-images.githubusercontent.com/81156510/275876808-f1126ae6-94fc-4d18-ba29-7b623edace66.png",
+    title: "Salient Feature System",
+    text: "Detect profanity in the sentences and perform saliency checking on a sentence-by-sentence basis",
+    view: "https://huggingface.co/spaces/srini047/asapp-hackathon",
+    source: "https://github.com/srini047/asapp-hackathon",
+  },
+  {
+    imgsrc:
+      "https://user-images.githubusercontent.com/81156510/213933187-2200364d-c15f-449c-a2e2-bfa563cccd18.png",
+    title: "Parking Assistant using OpenCV",
+    text: "Implemented a parking assistant that helps in finding parking lots in congested places using OpenCV, Mask RCNN, Twilio. Winner of Twilio Track Prize – Hack6iX                                                             ",
+    view: "https://devpost.com/software/parking-assistant-using-opencv",
+    source: "https://github.com/srini047/Hack6iX",
+  },
+  {
+    imgsrc:
+      "https://user-images.githubusercontent.com/81156510/180809076-f4426ece-c8d5-492e-b44f-a43177a5afb9.png",
+    title: "Recommendation Engine",
+    text: "Designed and implemented price recommendation application using Web Scraping, in a 2-person team using Python. Enables buyers to select the best product with a smaller number of clicks from Amazon and Flipkart; Finalist in Tech Expo, an annual Techfest Techniche, conducted by IIT Guwahati – 2021 (Online)",
+    view: "https://github.com/srini047/price-recommender-engine",
+    source: "https://github.com/srini047/price-recommender-engine",
+  },
+  {
+    imgsrc:
+      "https://user-images.githubusercontent.com/81156510/180811822-1476b05d-d389-4065-9a9d-26e366aa9f68.jpg",
+    title: "Sentiment Analysis using Python",
+    text: "Designed and implemented a text sentiment analyzer which classifies the nature of text as positive or negative using NLP. Runner Up in Hackathon, an annual Techfest Fiestaa 22, conducted by KPRIET – 2022 (In-person)",
+    view: "https://huggingface.co/spaces/srini047/text-based-sentiment-analyzer",
+    source: "https://github.com/srini047/text-based-sentiment-analysis",
+  },
+  {
+    imgsrc:
+      "https://user-images.githubusercontent.com/81156510/221420531-489f1381-e185-4630-965f-111d7d607c66.png",
+    title: "Travelogue",
+    text: "One-stop platform to connect with guides, & tourists to discover, explore, and interact with native folks and their culture. Used MERN stack to build the application and made use of Cohere and Twilio APIs to enhance the user experience and provide an AI touch to the application.",
+    view: "http://devpost.com/software/travelogue-q1eyzh",
+    source: "https://github.com/srini047/travelogue-backend",
+  },
+];
 
 export default ProjectCardData;
